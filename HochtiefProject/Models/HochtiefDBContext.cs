f89f1ee0294a4d2f13549e968da6d74ca6d60309
Med Yassin Azlouk <azlouknour@gmail.com>
2020-06-04 21:01:10 +0200

    Changes 04.06.2020

--- HochtiefProject/Models/HochtiefDBContext.cs
+++ HochtiefProject/Models/HochtiefDBContext.cs
@@ -1,11 +1,42 @@
-﻿using System;
+﻿
+
+/*using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace HochtiefProject.Models
+{
+   public class HochtiefDBContext : DbContext
+    {
+        public HochtiefDBContext(DbContextOptions<HochtiefDBContext> options):base(options)
+        {
+                
+    
+        }
+        public System.Data.Entity.DbSet<Labour_Test> labours { get; set; }
+    }
+
+
+}*/
+using HochtiefProject.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace HochtiefProject.Models
 {
-    public class HochtiefDBContext:DbContext 
+    public class HochtiefDBContext : DbContext
     {
+        public HochtiefDBContext(DbContextOptions<HochtiefDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Labour_Test> Authors { get; set; }
+    
     }
 }
--- HochtiefProject/Startup.cs
+++ HochtiefProject/Startup.cs
@@ -7,13 +7,21 @@ using HochtiefProject.Models.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 namespace HochtiefProject
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -22,6 +30,10 @@ namespace HochtiefProject
             services.AddMvc();
             services.AddSingleton<ILabourRepostroy<Labour_Test>, LabourRepostory>();
             services.AddMvc(options => options.EnableEndpointRouting = false);
+            services.AddDbContext<HochtiefDBContext>(options =>
+            {
+                options.(configuration.GetConnectionString("SqlVicon"));
+            }); 
 
         }
 
